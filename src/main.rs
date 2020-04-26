72a849ebe0c6292d8014bbddc58009b6026a1e4b
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-04-26 19:20:28 +0100

    update main function, tests will be done with cargo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
 pub mod serialize;
 
-use std::io::{self, BufReader};
-
-use serialize::{nbt::NBT, DataInput};
-
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let _stdin_lock = stdin.lock();
-    let stdin_lock = BufReader::new(_stdin_lock);
-
-    let mut input = DataInput::new(stdin_lock);
-    let nbt_tag = NBT::read_from(&mut input)?;
-
-    println!("{:#?}", nbt_tag);
-    Ok(())
+fn main() {
+    println!(":-)")
 }
